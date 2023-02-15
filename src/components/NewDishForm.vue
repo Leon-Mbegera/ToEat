90f1e9a3b3f89e2744ded2b8569d616ab35f0872
Leon-Mbegera <mbegeraleon89@gmail.com>
2023-02-15 12:00:48 +0300

    Convert NewDishForm to use composition API and compiler macros

--- src/components/NewDishForm.vue
+++ src/components/NewDishForm.vue
@@ -1,18 +1,22 @@
-<script>
-import { defineComponent, ref } from 'vue'
-import { v4 as uuidv4 } from 'uuid'
+<script setup lang="ts">
 
-export default defineComponent({
-  emits: ['add-new-dish', 'cancel-new-dish'],
-  data: () => ({
-    newDish: {
-      id: uuidv4(),
-      name: '',
-      status: 'Want to Try',
-      diet: '',
-    },
-  }),
+import { ref } from 'vue'
+import { v4 as uuidv4 } from 'uuid'
+import type { Dish } from '@/types'
+
+const newDish = ref<Dish>({
+  id: uuidv4(),
+  name: '',
+  status: 'Want to Try',
+  diet: '',
 })
+
+const emits = defineEmits<{
+  (e: 'add-new-dish', newDish: Dish): void
+  (e: 'cancel-new-dish'): void
+}>()
+
+
 </script>
 
 <template>
@@ -21,24 +25,20 @@ export default defineComponent({
       <div class="field">
         <label for="name" class="label">Name</label>
         <div class="control">
-          <input
-            v-model="newDish.name"
-            type="text"
-            class="input is-large"
-            placeholder="Mystery Flavored Shrimp"
-            required
-            ref="elNameInput"
-          />
+          <input v-model="newDish.name" type="text" class="input is-large" placeholder="Mystery Flavored Shrimp"
+            required ref="elNameInput" />
         </div>
       </div>
       <div class="field">
         <div class="buttons">
-          <button @click="$emit('add-new-dish', newDish)" class="button is-success">Create</button>
-          <button @click="$emit('cancel-new-dish')" class="button is-light">Cancel</button>
+          <button @click="emits('add-new-dish', newDish)" class="button is-success">Create</button>
+          <button @click="emits('cancel-new-dish')" class="button is-light">Cancel</button>
         </div>
       </div>
     </div>
   </form>
 </template>
 
-<style></style>
+<style>
+
+</style>
